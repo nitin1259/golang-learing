41942f65e40d6ee84b54dd3812a89dbdf3f52885
Nitin SINGH <nisingh@hyd-ml-00064410.olympus.f5net.com>
2019-11-28 19:32:19 +0530

    add more assertion to go test

--- src/cards/deck_test.go
+++ src/cards/deck_test.go
@@ -9,6 +9,12 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 12 {
-		t.Errorf("Expected deck length to be 12 but got %v", len(d))
+		t.Errorf("Expected deck length to be 12, but got %v", len(d))
+	} else if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be 'Ace of Spades', but got %v", d[0])
+	} else if d[3] != "Ace of Heart" {
+		t.Errorf("Expected first card to be 'Ace of Heart', but got %v", d[3])
+	} else if d[len(d)-1] != "Three of Clubs" {
+		t.Errorf("Expected first card to be 'Three of Clubs', but got %v", d[len(d)-1])
 	}
 }
