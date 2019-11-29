7b75182bbe8560c9f7330bb68559f7dadb2267a0
Nitin SINGH <nisingh@hyd-ml-00064410.olympus.f5net.com>
2019-11-29 20:34:50 +0530

    way of initialization struct

--- src/structs/main.go
+++ src/structs/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	// one way of initializing struct
+	alex := person{"Alex", "Jandon"}
+	fmt.Println(alex)
+
+	// another way of initialization struct
+	james := person{firstName: "James", lastName: "Anderson"}
+	fmt.Println(james)
 
 }
 
