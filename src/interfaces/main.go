05c7e30e43dd53c4dc0e7a46ee42e95da6858d02
Nitin SINGH <nisingh@hyd-ml-00064410.olympus.f5net.com>
2019-12-02 14:01:04 +0530

    adding another exaple with out func implementation of interface in russian bot

--- src/interfaces/main.go
+++ src/interfaces/main.go
@@ -4,37 +4,19 @@ import "fmt"
 
 /**
 
-is line of code right here can be interpreted as essentially saying hey everyone inside of this
-
-program everyone inside the program all of you different types out there all of you different types
-
-like English spot in Spanish box to all of you to whom it may concern our program now has a new type
-
-a third custom type called bot.
+This line below can be interpreted as essentially saying
+hey everyone inside of this program everyone inside the program all of you different types out there all of you different types
+like English bot in Spanish bot to all of you to whom it may concern our program now has a new type a third custom type called bot.
 
 So we have a new custom type called bot right here.
 
-Now there's nothing special about the named but I only use the named bot to kind of you know kind of
+So we're you can kind of imagine we are defining some behaviors right here that are common to all different bots that we might have.
 
-implement or kind of give you the idea of a more kind of generic approach to an English bot or Spanish
+So if we also had a Russian bot a on an old French box Italian bot and so on they all kind of share the same implementation from this type based bot right here.
 
-spot.
+So to all these custom types just know to whom it may concern. There's a new custom type called box and then inside the definition of this custom type we said if you
 
-So we're you can kind of imagine we are defining some behaviors right here that are common to all different
-
-bots that we might have.
-
-So if we also had a Russian bot a on an old French box Italian bot and so on they all kind of share
-
-the same implementation from this type based bot right here.
-
-So to all these custom types just know to whom it may concern.
-
-There's a new custom type called box and then inside the definition of this custom type we said if you
-
-are a type in this program with a function called get a greeting and you return a type of string then
-
-you are now also an honorary member of type bot.
+are a type in this program with a function called get a greeting and you return a type of string then you are now also an honorary member of type bot.
 */
 
 type bot interface {
@@ -47,6 +29,7 @@ func printGreeting(b bot) {
 
 type englishBot struct{}
 type spanishBot struct{}
+type russianBot struct{}
 
 func main() {
 	fmt.Println("Welcome to go interfaces")
@@ -55,7 +38,14 @@ func main() {
 
 	printGreeting(eb)
 	printGreeting(sb)
+
+	// rb := russianBot{}
+	// printGreeting(rb) will not work here untill unless I m not going to implement getGreeting func from interface with russian bot
 }
+
+// func (rb russianBot) getGreeting() string {
+// 	return "Hi From Russia!!!!"
+// }
 
 // func printGreeting(eb englishBot) {
 // 	fmt.Println(eb.getGreeting())
