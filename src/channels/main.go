296c5b7ad1b2e92a33a931110c41e50a090eefc8
Nitin SINGH <nisingh@hyd-ml-00064410.olympus.f5net.com>
2019-12-04 20:15:19 +0530

    intro of go routine and channel for inter communication b/w go routines

--- src/channels/main.go
+++ src/channels/main.go
@@ -18,17 +18,24 @@ func main() {
 
 	fmt.Println(links)
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		// checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " might be down")
+		c <- "Might be down link"
 		return
 	}
 
 	fmt.Println(link, " is up")
+	c <- "yup link is up!!!"
 }
